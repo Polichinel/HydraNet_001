fc61b6568b44f0a460fec4beb129f84cf437a0c7
Polichinel <spm@ifs.ku.dk>
2022-05-17 22:42:55 +0200

    try with my function again...

--- trainingLoopUtils.py
+++ trainingLoopUtils.py
@@ -123,11 +123,11 @@ def train(model, optimizer, criterion_reg, criterion_class, input_tensor, meta_t
             latitudes= meta_tensor_dict['latitudes'].to(device).reshape(-1).detach()
 
             # norm to between 0 and 1
-            longitudes_norm = (longitudes - longitudes.min())/(longitudes.max()-longitudes.min())
-            latitudes_norm = (latitudes - latitudes.min())/(latitudes.max()-latitudes.min())
+            #longitudes_norm = (longitudes - longitudes.min())/(longitudes.max()-longitudes.min())
+            #latitudes_norm = (latitudes - latitudes.min())/(latitudes.max()-latitudes.min())
 
-            #longitudes_norm = norm(longitudes, 0 ,1)
-            #latitudes_norm = norm(latitudes, 0 ,1)
+            longitudes_norm = norm(longitudes, 0 ,1)
+            latitudes_norm = norm(latitudes, 0 ,1)
 
             # NxD
             coords = torch.column_stack([longitudes_norm, latitudes_norm])
