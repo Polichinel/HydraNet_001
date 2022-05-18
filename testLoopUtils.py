bb60204eec4f25c6d07117d40696d4dfc14d1759
Polichinel <spm@ifs.ku.dk>
2022-05-18 21:09:04 +0200

    standard normal

--- testLoopUtils.py
+++ testLoopUtils.py
@@ -26,7 +26,7 @@ def unit_norm(x, noise = False):
     x_unit_norm = x / np.linalg.norm(x)
 
     if noise == True:
-        x_unit_norm += np.random.normal(loc = 0, scale = x_unit_norm.std(), size = len(x_unit_norm))
+        x_unit_norm += np.random.standard_normal(len(x_unit_norm))
 
     return(x_unit_norm)
 
