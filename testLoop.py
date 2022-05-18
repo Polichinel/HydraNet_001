ff659d48c53bd5d0e3aa5b3e6b2c22d34ab4dd68
Polichinel <spm@ifs.ku.dk>
2022-05-18 20:46:29 +0200

    unit norm test

--- testLoop.py
+++ testLoop.py
@@ -115,8 +115,15 @@ latitudes = ucpd_vol[0 ,  :  ,  : , 2].reshape(-1)
 #longitudes_norm = torch.tensor((longitudes - longitudes.min())/(longitudes.max()-longitudes.min()), dtype = torch.float).to(device)#.detach()
 #latitudes_norm = torch.tensor((latitudes - latitudes.min())/(latitudes.max()-latitudes.min()), dtype = torch.float).to(device)#.detach()
 
-longitudes_norm = torch.tensor(norm(longitudes, 0 ,1), dtype = torch.float).to(device)#.detach()
-latitudes_norm = torch.tensor(norm(latitudes, 0 ,1), dtype = torch.float).to(device)#.detach()
+# longitudes_norm = torch.tensor(norm(longitudes, 0 ,1), dtype = torch.float).to(device)#.detach()
+# latitudes_norm = torch.tensor(norm(latitudes, 0 ,1), dtype = torch.float).to(device)#.detach()
+
+longitudes_norm = torch.tensor(unit_norm(longitudes), dtype = torch.float).to(device)#.detach()
+latitudes_norm = torch.tensor(unit_norm(latitudes), dtype = torch.float).to(device)#.detach()
+
+# longitudes_norm = torch.tensor(unit_norm(longitudes, noise= True), dtype = torch.float).to(device)#.detach()
+# latitudes_norm = torch.tensor(unit_norm(latitudes, noise= True), dtype = torch.float).to(device)#.detach()
+
 
 # NxD
 coords = torch.column_stack([longitudes_norm, latitudes_norm])
