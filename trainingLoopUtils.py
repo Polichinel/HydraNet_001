7542c021be69741e34d65b7a2b1c1f1d3ac09077
Polichinel <spm@ifs.ku.dk>
2022-05-19 09:44:33 +0200

    long norm

--- trainingLoopUtils.py
+++ trainingLoopUtils.py
@@ -147,8 +147,8 @@ def train(model, optimizer, criterion_reg, criterion_class, input_tensor, meta_t
             #longitudes_norm = (longitudes - longitudes.min())/(longitudes.max()-longitudes.min())
             #latitudes_norm = (latitudes - latitudes.min())/(latitudes.max()-latitudes.min())
 
-            #longitudes_norm = norm(longitudes, 0 ,1).detach() # detaching helped!
-            #latitudes_norm = norm(latitudes, 0 ,1).detach()
+            longitudes_norm = norm(longitudes, 0 ,1).detach() # detaching helped!
+            latitudes_norm = norm(latitudes, 0 ,1).detach()
 
 
             #longitudes_norm = unit_norm(longitudes, noise= True).detach() # detaching helped!
