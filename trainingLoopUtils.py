ac9380311ccf3b8096592ddebffcc3892e1081b8
Polichinel <spm@ifs.ku.dk>
2022-09-20 23:57:08 +0200

    see if this works..

--- trainingLoopUtils.py
+++ trainingLoopUtils.py
@@ -96,7 +96,7 @@ def get_input_tensors(ucpd_vol):
 def train(model, optimizer, criterion_reg, criterion_class, input_tensor, meta_tensor_dict, device, unet, plot = False):
 
     # Tell wandb to watch what the model gets up to: gradients, weights, and more!
-    wandb.watch(model, criterion_reg, criterion_class, log="all", log_freq=10)
+    wandb.watch(model, [criterion_reg, criterion_class], log="all", log_freq=10)
     
     avg_reg_loss = 0
     avg_class_loss = 0
