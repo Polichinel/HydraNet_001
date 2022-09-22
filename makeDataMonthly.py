a3580a1a8c071736e29d76a64b7c7446bbe00377
Polichinel <spm@ifs.ku.dk>
2022-09-22 02:49:26 +0200

    maybe it works?

--- makeDataMonthly.py
+++ makeDataMonthly.py
@@ -1,11 +1,10 @@
 import numpy as np
 import pandas as pd
 import geopandas as gpd
-from shapely.geometry import Point
-
 import pickle
 import urllib.request
 import os
+
 
 def get_gwno():
 
@@ -31,6 +30,8 @@ def get_prio_shape():
         print('File already downloaded')
         prio_grid = gpd.read_file('zip://' + path_prio)
 
+        prio_grid =  pd.DataFrame(prio_grid.drop(columns = ['geometry']))
+
     else:
         print('Beginning file download PRIO...')
         url_prio = 'http://file.prio.no/ReplicationData/PRIO-GRID/priogrid_shapefiles.zip'
@@ -38,7 +39,7 @@ def get_prio_shape():
         urllib.request.urlretrieve(url_prio, path_prio)
         prio_grid = gpd.read_file('zip://' + path_prio)
 
-        prio_grid =  pd.DataFrame(prio_grid.drop(columns = ['geometry'])) #do not need the geom here.
+        prio_grid =  pd.DataFrame(prio_grid.drop(columns = ['geometry']))
 
     return prio_grid
 
@@ -59,13 +60,13 @@ def get_gwno():
 
 def get_ucdp():
 
-    location = '/home/projects/ku_00017/data/raw/UCDP'
-    #location = '/home/simon/Documents/Bodies/data/UCDP' #local
+    #location = '/home/projects/ku_00017/data/raw/UCDP'
+    location = '/home/simon/Documents/Bodies/data/UCDP' #local
     path_ucdp = location + "/ged201-csv.zip"
     
     if os.path.isfile(path_ucdp) == True:
         print('file already downloaded')
-        ucdp = pd.read_csv(path_ucdp, low_memory=False) # see if this removes dtype warning
+        ucdp = pd.read_csv(path_ucdp, low_memory=False)
 
 
     else: 
@@ -76,65 +77,80 @@ def get_ucdp():
         urllib.request.urlretrieve(url_ucdp, path_ucdp)
         ucdp = pd.read_csv(path_ucdp, low_memory=False)
 
-    return ucdp
 
-def add_months(ucdp, world_grid):
+    # just to save ram for now !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # sub_years = sorted(ucdp['year'].unique())[:2]
+    # ucdp = ucdp[ucdp['year'].isin(sub_years)]
+    # -----------------------------------------------------------------------------------------------------
 
-    diff = ucdp['year'].max() - world_grid['year'].max()
+    ucdp['month'] = pd.to_datetime(ucdp['date_start']).dt.month
+    ucdp.rename(columns= {'priogrid_gid' : 'gid'}, inplace= True)
 
-    subset_list = []
+    feature_list = ['month','year', 'gid', 'deaths_a','deaths_b', 'deaths_civilians', 'deaths_unknown','best', 'high', 'low']
 
-    for i in np.arange(1, diff+1, 1):
-
-        subset = world_grid[world_grid['year'] == world_grid['year'].max()].copy()
-        subset['year'] = world_grid['year'].max() + i
-
-        subset_list.append(subset)
-
-    new_years = pd.concat(subset_list)
-    world_grid_all_years = pd.concat([world_grid, new_years])
-
-    month = [str(i).zfill(2) for i in np.arange(1,13,1)]
-    world_grid_all_years.loc[:,'month'] = world_grid_all_years.apply(lambda _: month, axis=1)
-    world_grid_all_months = world_grid_all_years.sort_values('year').explode('month').copy()
-    world_grid_all_months['year_months_start'] =  world_grid_all_months['year'].astype(str) + '-' +  world_grid_all_months['month'].astype(str)
-
-    year_months = sorted(world_grid_all_months['year_months_start'].unique())
-    ts = len(year_months)
-    month_ids = np.arange(109, ts + 109, 1)
-    month_id_dict = dict(zip(year_months,month_ids))
-    month_df = pd.DataFrame({'year_months_start' : year_months, 'month_id': month_ids})
-    world_grid_all_months_id = world_grid_all_months.merge(month_df, how = 'left', on = 'year_months_start')
-
-    return world_grid_all_months_id
-
-
-def prio_ucdp_merge(ucdp, world_grid_all_months):
-    ucdp_tmp1 = ucdp.copy()
-
-    ucdp_tmp1['year_months_start'] = ucdp_tmp1['date_start'].str.slice(start = 0, stop = 7) # Date YYYY-MM-DD
-    ucdp_tmp1['year_months_end'] = ucdp_tmp1['date_start'].str.slice(start = 0, stop = 7) # Date YYYY-MM-DD
-
-
-    mask1 = (ucdp_tmp1['year'] != ucdp_tmp1['year_months_start'].str.slice(start = 0, stop = 4).astype(int))
-    mask2 = (ucdp_tmp1['year'] != ucdp_tmp1['year_months_end'].str.slice(start = 0, stop = 4).astype(int))
-
-    # correction. Note that end and start year for the four entries that is corrected is the same.
-    ucdp_tmp1.loc[mask1 | mask2, 'year'] = ucdp_tmp1.loc[mask1 | mask2,'year_months_start'].str.slice(start = 0, stop = 4).astype(int)
-
-    feature_list = ['deaths_a','deaths_b', 'deaths_civilians', 'deaths_unknown','best', 'high', 'low']
-
-    ucdp_monthly_unit = ucdp_tmp1.groupby(['year_months_start','year', 'priogrid_gid']).sum()[feature_list].reset_index()
-    ucdp_monthly_unit.rename(columns={'priogrid_gid':'gid'}, inplace=True)
-
+    ucdp_monthly_unit = ucdp.loc[:,feature_list].groupby(['month','year', 'gid']).sum().reset_index()
     ucdp_monthly_unit['log_best'] = np.log(ucdp_monthly_unit['best'] +1)
     ucdp_monthly_unit['log_low'] = np.log(ucdp_monthly_unit['low'] +1)
     ucdp_monthly_unit['log_high'] = np.log(ucdp_monthly_unit['high'] +1)
 
-    prio_ucdp_df = world_grid_all_months.merge(ucdp_monthly_unit, how = 'left', on = ['gid', 'year_months_start', 'year'])
-    prio_ucdp_df.fillna(0, inplace=True)
+    ucdp_monthly_unit['in_ucdp'] = True # handy later when I wnat to remove water and stuff.
 
-    return prio_ucdp_df
+    return ucdp_monthly_unit
+
+
+def monthly_grid(prio_grid, ucdp):
+
+    years = [sorted(ucdp['year'].unique())] * prio_grid.shape[0]
+    months = [list(np.arange(1, 13))] * prio_grid.shape[0]
+
+    prio_grid['year'] = years
+    prio_grid['month'] = months
+
+    prio_grid = prio_grid.explode('year').reset_index(drop=True) 
+    prio_grid = prio_grid.explode('month').reset_index(drop=True) 
+
+    prio_grid['year'] = prio_grid['year'].astype(int)
+    prio_grid['month'] = prio_grid['month'].astype(int)
+
+    return prio_grid
+
+
+def make_full_grid(prio_grid, ucdp, gwno):
+
+    prio_grid = prio_grid.merge(gwno, on = ['gid', 'year'], how = 'left') 
+
+    # you can easily add monthly prio features here (if you got the ram for it)
+
+    full_grid = prio_grid.merge(ucdp, on = ['gid', 'year', 'month'], how = 'left') 
+    full_grid.fillna({'best' : 0, 'low' : 0, 'high' : 0, 'log_best' : 0, 'log_low' : 0, 'log_high' : 0, 'gwno' : 0}, inplace = True) # for gwno 0 is no country
+    full_grid.fillna({'in_ucdp' : False}, inplace = True)
+
+    full_grid['log_best'] = np.log(full_grid['best'] + 1)
+    full_grid['log_high'] = np.log(full_grid['high'] + 1)
+    full_grid['log_low'] = np.log(full_grid['low'] + 1)
+
+    return full_grid
+
+
+def add_month_id(prio_grid):
+
+    prio_grid['year_month'] = prio_grid['year'].astype(str) + '_' + prio_grid['month'].astype(str) 
+
+    ts = prio_grid['year_month'].unique()
+    n_ts = len(ts)
+    month_ids = np.arange(109, n_ts + 109, 1)
+    month_id_df = pd.DataFrame({'year_month' : ts, 'month_id': month_ids})
+    prio_grid = prio_grid.merge(month_id_df, on = 'year_month', how = 'left')
+
+    return prio_grid
+
+def check_month_id(prio_grid):
+
+    for year in prio_grid['year'].unique():
+        print(year, end = ': ')
+        print(prio_grid[prio_grid['year'] == year]['month_id'].unique(), end = ': ')
+        print(len(prio_grid[prio_grid['year'] == year]['month_id'].unique()))
+
 
 def make_volumn(df):
 
@@ -155,32 +171,28 @@ def make_volumn(df):
 
     return ucpd_vol
 
-def get_prio_ucdp():
-
-    prio_grid = get_prio_shape()
-    gwno = get_gwno()
-    ucdp = get_ucdp()
-
-    #world_grid = prio_grid.merge(gwno, how = 'right', on = 'gid') # if you just merge this on outer I think you get the full grid needed for R-UNET
-    world_grid = prio_grid.merge(gwno, how = 'outer', on = 'gid') # if you just merge this on outer I think you get the full grid needed for R-UNET
-    world_grid.fillna({'best' : 0, 'low' : 0, 'high' : 0, 'log_best' : 0, 'log_low' : 0, 'log_high' : 0}, inplace = True)
-
-    world_grid_all_months = add_months(ucdp, world_grid)
-    prio_ucdp = prio_ucdp_merge(ucdp, world_grid_all_months)
-
-    return prio_ucdp
 
 
 def compile():
 
-    print('Getting data')
-    prio_ucdp = get_prio_ucdp()
+    prio_grid = get_prio_shape()
+    gwno = get_gwno()
+    ucdp = get_ucdp()
+    prio_grid = monthly_grid(prio_grid, ucdp)
+    prio_grid = make_full_grid(prio_grid, ucdp, gwno)
+    prio_grid = add_month_id(prio_grid)
+    print(prio_grid.shape)
 
-    print('Making volumn')
-    ucpd_vol = make_volumn(prio_ucdp)
+    check_month_id(prio_grid)
+
+    ucpd_vol = make_volumn(prio_grid)
+    print(ucpd_vol.shape)
+
 
     location = '/home/projects/ku_00017/data/raw/conflictNet'
     #location = '/home/simon/Documents/Articles/ConflictNet/data/raw'
+
+
 
     print('Saving pickle')
     file_name = "/ucpd_monthly_vol.pkl"
@@ -189,7 +201,3 @@ def compile():
     output.close()
 
     print('Done')
-
-
-if __name__ == '__main__':
-    compile()
