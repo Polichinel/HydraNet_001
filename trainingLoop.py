ba123717cd7ab01f36c52ab5b4c9975cb9ff5e96
Polichinel <spm@ifs.ku.dk>
2022-09-22 08:58:07 +0200

    does it work for year?

--- trainingLoop.py
+++ trainingLoop.py
@@ -201,7 +201,9 @@ def end_test(unet, ucpd_vol, config):
     y_score_prob = t31_pred_class_np_mean.reshape(360*720) # way better brier!
 
     # y_true = ucpd_vol[30,:,:,4].reshape(360*720) # 7 not 4 when you do sinkhorn and have coords 
-    y_true = ucpd_vol[30,:,:,7].reshape(360*720)
+#    y_true = ucpd_vol[30,:,:,7].reshape(360*720)
+    y_true = ucpd_vol[-1,:,:,7].reshape(360*720)
+
 
     y_true_binary = (y_true > 0) * 1
 
