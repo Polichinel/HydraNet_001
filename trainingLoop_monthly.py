be7bb46895bc3feec6c0fde61895679cad666a05
Polichinel <spm@ifs.ku.dk>
2022-09-22 03:13:08 +0200

    comment

--- trainingLoop_monthly.py
+++ trainingLoop_monthly.py
@@ -149,7 +149,7 @@ def test(model, input_tensor, device):
 def get_posterior(unet, ucpd_vol, device, n):
 
   #ttime_tensor = torch.tensor(ucpd_vol[:, :, : , 4].reshape(1, 31, 360, 720)).float().to(device) #Why not do this in funciton?
-  ttime_tensor = torch.tensor(ucpd_vol[:, :, : , 7].reshape(1, 31, 360, 720)).float().to(device) #7 not 4 when you do sinkhorn
+  ttime_tensor = torch.tensor(ucpd_vol[:, :, : , 7].reshape(1, 31, 360, 720)).float().to(device) #log best is 7 not 4 when you do sinkhorn or just have coords.
   
   pred_list = []
   pred_list_class = []
