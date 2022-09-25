e096acf4ebab3e7981cfae14e4e8550302a2a7fb
Polichinel <spm@ifs.ku.dk>
2022-09-25 16:00:22 +0200

    ...

--- trainingLoop_monthly.py
+++ trainingLoop_monthly.py
@@ -274,7 +274,7 @@ if __name__ == "__main__":
     'betas' : (0.9, 0.999),
     "epochs": 2, # as it is now, this is samples...
     "batch_size": 8, # this also you do not ues
-    "samples" : 150,
+    "samples" : 140,
     "test_samples": 16, # go 128, but this is tj́sut to see is there is a collaps
     "min_events": 24}
 
