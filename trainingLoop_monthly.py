89f625eabdf37bdafc381770b94474918e3bad68
Polichinel <spm@ifs.ku.dk>
2022-09-25 19:27:56 +0200

    this?

--- trainingLoop_monthly.py
+++ trainingLoop_monthly.py
@@ -194,7 +194,7 @@ def get_posterior(unet, ucpd_vol, device, n):
     test_tensor = torch.tensor(ucpd_vol[:, :, : , 7].reshape(1, -1, 360, 720)).float()#.to(device) #log best is 7 not 4 when you do sinkhorn or just have coords.    
     print(test_tensor.shape)
     
-    out_of_sample_tensor = test_tensor[-36:]
+    out_of_sample_tensor = test_tensor[:,-36:,:,:]
     print(out_of_sample_tensor.shape)
 
     posterior_list = []
