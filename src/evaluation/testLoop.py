b6dcea300f25ad1093d2125c31cd2994c8a6ad42
Polichinel <simmaa@prio.org>
2023-01-08 20:02:38 +0100

    New structure... scary

---
+++ src/evaluation/testLoop.py
@@ -0,0 +1,112 @@
+import numpy as np
+import random
+import pickle
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import brier_score_loss
+
+import wandb
+
+from testLoopUtils import *
+from recurrentUnet import *
+
+#import geomloss
+
+start_t = time.time()
+
+
+loss_arg = input(f'a) Sinkhorn \nb) BCE/MSE \n')
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+print('loading data....')
+#location = '/home/simon/Documents/Articles/ConflictNet/data/raw'
+location = '/home/projects/ku_00017/data/raw/conflictNet'
+file_name = "/ucpd_vol.pkl"
+pkl_file = open(location + file_name, 'rb')
+ucpd_vol = pickle.load(pkl_file)
+pkl_file.close()
+
+
+print('loading model....')
+
+hidden_channels = 64
+input_channels = 1
+output_channels = 1
+dropout_rate = 0.5
+
+unet = UNet(input_channels, hidden_channels, output_channels, dropout_rate).to(device)
+
+if loss_arg == 'a':
+    PATH = 'unet_sinkhorn.pth'
+
+elif loss_arg == 'b':
+    PATH = 'unet.pth'
+
+unet.load_state_dict(torch.load(PATH))
+#unet.eval() # you do this some other place right?
+
+
+pred_list, pred_list_class = get_posterior(unet, ucpd_vol, device, n=100)
+
+# reg statistics
+print('reg')
+t31_pred_np = np.array(pred_list)
+
+t31_pred_np_mean = t31_pred_np.mean(axis=0)
+print(t31_pred_np.min(), t31_pred_np.max())
+
+t31_pred_np_std = t31_pred_np.std(axis=0)
+print(t31_pred_np_std.min(), t31_pred_np_std.max())
+
+# Class statistics - right noe this does not get updated through backprob..
+print('class')
+t31_pred_class_np = np.array(pred_list_class)
+
+t31_pred_class_np_mean = t31_pred_class_np.mean(axis=0)
+print(t31_pred_class_np.min(), t31_pred_class_np.max()) # the minimun is very high!
+
+t31_pred_class_np_std = t31_pred_class_np.std(axis=0)
+print(t31_pred_class_np_std.min(), t31_pred_class_np_std.max())
+
+
+
+# Classification results
+y_var = t31_pred_np_std.reshape(360*720)
+y_score = t31_pred_np_mean.reshape(360*720)
+
+# HERE
+#y_score_prob = torch.sigmoid(torch.tensor(y_score)) # old trick..
+y_score_prob = t31_pred_class_np_mean.reshape(360*720) # way better brier!
+
+
+# y_true = ucpd_vol[30,:,:,4].reshape(360*720) # 7 not 4 when you do sinkhorn and have coords 
+y_true = ucpd_vol[30,:,:,7].reshape(360*720)
+
+y_true_binary = (y_true > 0) * 1
+
+print('Unet')
+
+print(mean_squared_error(y_true, y_score))
+print(average_precision_score(y_true_binary, y_score_prob))
+print(roc_auc_score(y_true_binary, y_score_prob))
+print(brier_score_loss(y_true_binary, y_score_prob))
+
+end_t = time.time()
+minutes = (end_t - start_t)/60
+
+print(f'Done. Runtime: {minutes:.3f} minutes')
+
+
+# TODO:
+# Ppersitance model should go back in and yo should output in a .txt fil.
---
+++ src/evaluation/testLoopUtils.py
@@ -0,0 +1,89 @@
+import numpy as np
+import random
+import pickle
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import brier_score_loss
+
+import wandb
+
+
+def norm(x, a = 0, b = 1):
+
+    """Return a normalized x in range [a:b]. Default is [0:1]. Used for coordinates"""
+    x_norm = (b-a)*(x - x.min())/(x.max()-x.min())+a
+    return(x_norm)
+
+def unit_norm(x, noise = False):
+
+    """Return a normalized x (unit vector). Used for coordinates"""
+    x_unit_norm = x / np.linalg.norm(x)
+
+    if noise == True:
+        x_unit_norm += np.random.normal(1, 2*x_unit_norm.std(), len(x_unit_norm))
+
+    return(x_unit_norm)
+
+def standard(x, noise = False):
+
+    """Return a standardnized x. Used for coordinates"""
+    x_standard = (x - x.mean()) / x.std()
+
+    if noise == True:
+        x_unit_norm += np.random.normal(loc = 0, scale = x_standard.std(), size = len(x_standard))
+
+    return(x_standard)
+
+def apply_dropout(m):
+    if type(m) == nn.Dropout:
+        m.train()
+
+def test(model, input_tensor, device):
+  model.eval() # remove to allow dropout to do its thing as a poor mans ensamble. but you need a high dropout..
+  model.apply(apply_dropout)
+  # but there was also something else that you neede to acount for when doing this..?
+
+  h_tt = model.init_hTtime(hidden_channels = model.base).float().to(device)
+  seq_len = input_tensor.shape[1] 
+  H = input_tensor.shape[2]
+  W = input_tensor.shape[3] 
+
+  for i in range(seq_len-1): # need to get hidden state...
+
+    t0 = input_tensor[:, i, :, :].reshape(1, 1 , H , W).to(device) 
+    t1 = input_tensor[:, i+1, :, :].reshape(1, 1 , H, W).to(device)
+
+    t1_pred, t1_pred_class, h_tt = model(t0, h_tt)
+
+  # You only want the last one
+  tn_pred_np = t1_pred.cpu().detach().numpy() # so yuo take the final pred..
+  tn_pred_class_np = t1_pred_class.cpu().detach().numpy() # so yuo take the final pred..
+
+  return tn_pred_np, tn_pred_class_np
+
+def get_posterior(model, ucpd_vol, device, n=100):
+
+  #ttime_tensor = torch.tensor(ucpd_vol[:, :, : , 4].reshape(1, 31, 360, 720)).float().to(device) #Why not do this in funciton?
+  ttime_tensor = torch.tensor(ucpd_vol[:, :, : , 7].reshape(1, 31, 360, 720)).float().to(device) #7 not 4 when you do sinkhorn
+  
+  pred_list = []
+  pred_list_class = []
+
+  for i in range(n):
+    t31_pred_np, tn_pred_class_np = test(model, ttime_tensor, device)
+    pred_list.append(t31_pred_np)
+    pred_list_class.append(tn_pred_class_np)
+
+    if i % 10 == 0: # print steps 10
+        print(f'{i}')
+
+  return pred_list, pred_list_class
