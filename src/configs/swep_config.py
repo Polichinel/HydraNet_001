b75d7236df5044b307c496bcdefda29aa58dd4e2
Polichinel <simmaa@prio.org>
2023-01-25 12:45:57 +0100

    new run

--- src/configs/swep_config.py
+++ src/configs/swep_config.py
@@ -14,7 +14,7 @@ def get_swep_config():
     parameters_dict = {
         'hidden_channels': {'values': [16, 24]},
         'min_events': {'values': [16, 20]},
-        'samples': {'values': [200, 250]},
+        'samples': {'values': [300, 350]},
         "dropout_rate" : {'value' : 0.1},# {'values' : [0.05, 0.1]},
         'learning_rate': {'value' :  0.00005},# {'values' : [0.00001, 0.00005]},
         "weight_decay" : {'value' : 0.1},#{'values' : [0.1, 0.05]},
