6b521ac24bc2763000be02c76dea5c9412c88b66
Polichinel <simmaa@prio.org>
2023-01-26 13:38:11 +0100

    3 input

--- src/configs/hyperparameters_config.py
+++ src/configs/hyperparameters_config.py
@@ -8,7 +8,7 @@ def get_hp_config():
     "dropout_rate" : 0.05,
     'learning_rate' :  0.00005,
     "weight_decay" :  0.1,
-    'input_channels' : 1,
+    'input_channels' : 3,
     "output_channels" : 1,
     "loss": 'b', 
     "test_samples": 128}
