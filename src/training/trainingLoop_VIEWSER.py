8470777fe9fa14225f8f862fc7d6a6de0e319608
Polichinel <simmaa@prio.org>
2023-01-28 19:47:49 +0100

    this?

--- src/training/trainingLoop_VIEWSER.py
+++ src/training/trainingLoop_VIEWSER.py
@@ -110,16 +110,27 @@ def train(model, optimizer, criterion_reg, criterion_class, multitaskloss_instan
         # ------------------------------------------------------------------------------------------------------
 
         # forward-pass
-        loss_reg = criterion_reg(t1_pred[:,0,:,:], t1[:,0,:,:]) + criterion_reg(t1_pred[:,1,:,:], t1[:,1,:,:]) + criterion_reg(t1_pred[:,2,:,:], t1[:,2,:,:])
-        loss_class = criterion_class(t1_pred_class[:,0,:,:], t1_binary[:,0,:,:]) + criterion_class(t1_pred_class[:,1,:,:], t1_binary[:,1,:,:]) + criterion_class(t1_pred_class[:,2,:,:], t1_binary[:,2,:,:])  
+
+        loss1r = criterion_reg(t1_pred[:,0,:,:], t1[:,0,:,:])
+        loss2r = criterion_reg(t1_pred[:,1,:,:], t1[:,1,:,:])
+        loss3r = criterion_reg(t1_pred[:,2,:,:], t1[:,2,:,:])
+
+        loss1c = criterion_class(t1_pred_class[:,0,:,:], t1_binary[:,0,:,:])
+        loss2c = criterion_class(t1_pred_class[:,1,:,:], t1_binary[:,1,:,:])
+        loss3c = criterion_class(t1_pred_class[:,2,:,:], t1_binary[:,2,:,:]) 
+
+        # loss_reg = criterion_reg(t1_pred[:,0,:,:], t1[:,0,:,:]) + criterion_reg(t1_pred[:,1,:,:], t1[:,1,:,:]) + criterion_reg(t1_pred[:,2,:,:], t1[:,2,:,:])
+        # loss_class = criterion_class(t1_pred_class[:,0,:,:], t1_binary[:,0,:,:]) + criterion_class(t1_pred_class[:,1,:,:], t1_binary[:,1,:,:]) + criterion_class(t1_pred_class[:,2,:,:], t1_binary[:,2,:,:])  
         # loss = loss_reg + loss_class # naive no weights und so weider
 
         #loss = loss_reg + loss_class # naive no weights und so weider
         
-        losses = torch.stack((loss_reg, loss_class))
+        #losses = torch.stack((loss_reg, loss_class))
+        losses = torch.stack((loss1r, loss2r, loss3r, loss1c, loss2c, loss3c))
+        
         loss = multitaskloss_instance(losses)
 
-        print(loss.shape)
+        #print(loss.shape)
         # ------------------------------------------------------------------------------------------------------
 
 
