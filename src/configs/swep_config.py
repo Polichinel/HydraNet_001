97820892100ea033582042c2651a5602b3e46e43
Polichinel <simmaa@prio.org>
2023-02-07 20:20:50 +0100

    UNet

--- src/configs/swep_config.py
+++ src/configs/swep_config.py
@@ -11,7 +11,7 @@ def get_swep_config():
     sweep_config['metric'] = metric
 
     parameters_dict = {
-        'model' : {'value' : 'BNUNet'},
+        'model' : {'value' : 'UNet'},
         'clip_grad_norm' : {'value': True},
         'scheduler' : {'values': ['step', 'linear', None]},
         'hidden_channels': {'value': 16},
