76a55823b5d53863aa9c5167adf671396d06cdb3
Polichinel <simmaa@prio.org>
2023-02-08 09:38:27 +0100

    trying for a pickle

--- src/configs/hyperparameters_config.py
+++ src/configs/hyperparameters_config.py
@@ -4,16 +4,17 @@ def get_hp_config():
     hyperparameters = {
     "model" : 'BNUNet',
     "clip_grad_norm" : True,
-    "scheduler" : None,
+    "scheduler" : 'step',
     "hidden_channels" : 16,
-    "min_events" : 10,
-    "samples": 400, # 10 just for debug n
+    "min_events" : 20,
+    "samples": 800, # 10 just for debug n
     "dropout_rate" : 0.05,
-    'learning_rate' :  0.00001,
+    'learning_rate' :  0.0001,
     "weight_decay" :  0.1,
     'input_channels' : 3,
     "output_channels" : 3,
-    "loss": 'c', 
+    "loss_reg": 'c',
+    "loss_class": 'a', 
     "test_samples": 128}
 
     return hyperparameters
