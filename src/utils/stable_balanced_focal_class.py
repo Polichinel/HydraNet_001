6a4d2cfa4ace19b385c2b68ebbc1a64ad480a5be
Polichinel <simmaa@prio.org>
2023-02-21 23:33:35 +0100

    ...

--- src/utils/stable_balanced_focal_class.py
+++ src/utils/stable_balanced_focal_class.py
@@ -26,8 +26,8 @@ class stableBalancedFocalLossClass(nn.Module):
         input, target = input.unsqueeze(0), target.unsqueeze(0)
 
         # fo   r probs
-        min_ind = torch.exp(torch.tensor(-100)) # almost 0
-        max_ind = torch.tensor(1.0)- torch.exp(torch.tensor(-10)) # almost 1
+        min_ind = torch.exp(torch.tensor(-100).to(device)) # almost 0
+        max_ind = torch.tensor(1.0).to(device)- torch.exp(torch.tensor(-10).to(device)) # almost 1
         input = torch.clamp(input, min = min_ind, max = max_ind) # so we do not log(0) or log(1) due to under- or overflow
 
         pos = (-self.alpha * (1-input)**self.gamma * torch.log(input))
