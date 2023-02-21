d105c639cc6100cace3bb72b2c02c98313dd4170
Polichinel <simmaa@prio.org>
2023-02-21 23:31:44 +0100

    ..

--- src/configs/hyperparameters_config.py
+++ src/configs/hyperparameters_config.py
@@ -13,10 +13,10 @@ def get_hp_config():
     "weight_decay" :  0.1,
     'input_channels' : 3,
     "output_channels" : 3,
-    "loss_class": 'b',  # This is still unstable... 
+    "loss_class": 'c',  # This is still unstable... 
     'loss_class_gamma' : 1, # try with 1 here and 0.75 below # # gamma=0 no wieght. Between 0 and 1 seems very unstable... In general, this here above 0 seems unstable... At least the the reg values below 
     'loss_class_alpha' : 0.75, # try with 0.75 # alpha=0.5 even.
-    "loss_reg": 'c',
+    "loss_reg": 'b',
     'loss_reg_a' : 0.5, # new 
     'loss_reg_c' :  0.05, # new
     "test_samples": 128}
