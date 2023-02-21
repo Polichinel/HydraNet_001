c0e6b91e2b66da1e6ad141740cf51e2d2dff12ec
Polichinel <simmaa@prio.org>
2023-02-21 23:59:24 +0100

    sweep 1/4

--- src/configs/hyperparameters_config.py
+++ src/configs/hyperparameters_config.py
@@ -7,7 +7,7 @@ def get_hp_config():
     "scheduler" : 'step',
     "hidden_channels" : 16,
     "min_events" : 10,
-    "samples": 600, # 10 just for debug n
+    "samples": 400, # 10 just for debug n
     "dropout_rate" : 0.05,
     'learning_rate' :  0.0001,
     "weight_decay" :  0.1,
