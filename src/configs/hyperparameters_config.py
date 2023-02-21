47589e2cebfa275d886b46e6ac86939566402b49
Polichinel <simmaa@prio.org>
2023-02-21 23:29:24 +0100

    hybra1 and stable

--- src/configs/hyperparameters_config.py
+++ src/configs/hyperparameters_config.py
@@ -2,7 +2,7 @@
 def get_hp_config():
     
     hyperparameters = {
-    "model" : 'HydraBNUNet02', #'BNUNet',
+    "model" : 'HydraBNUNet01', #'BNUNet',
     "clip_grad_norm" : True,
     "scheduler" : 'step',
     "hidden_channels" : 16,
@@ -16,7 +16,7 @@ def get_hp_config():
     "loss_class": 'b',  # This is still unstable... 
     'loss_class_gamma' : 1, # try with 1 here and 0.75 below # # gamma=0 no wieght. Between 0 and 1 seems very unstable... In general, this here above 0 seems unstable... At least the the reg values below 
     'loss_class_alpha' : 0.75, # try with 0.75 # alpha=0.5 even.
-    "loss_reg": 'b',
+    "loss_reg": 'c',
     'loss_reg_a' : 0.5, # new 
     'loss_reg_c' :  0.05, # new
     "test_samples": 128}
