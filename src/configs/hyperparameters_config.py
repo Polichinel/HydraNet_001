fa0ae09549070f0910f59f3d6cf7f269e7315b28
Polichinel <simmaa@prio.org>
2023-02-21 23:20:04 +0100

    tryin hydra 2

--- src/configs/hyperparameters_config.py
+++ src/configs/hyperparameters_config.py
@@ -2,7 +2,7 @@
 def get_hp_config():
     
     hyperparameters = {
-    "model" : 'HydraBNUNet', #'BNUNet',
+    "model" : 'HydraBNUNet02', #'BNUNet',
     "clip_grad_norm" : True,
     "scheduler" : 'step',
     "hidden_channels" : 16,
@@ -13,7 +13,7 @@ def get_hp_config():
     "weight_decay" :  0.1,
     'input_channels' : 3,
     "output_channels" : 3,
-    "loss_class": 'c',  # This is still unstable... 
+    "loss_class": 'b',  # This is still unstable... 
     'loss_class_gamma' : 1, # try with 1 here and 0.75 below # # gamma=0 no wieght. Between 0 and 1 seems very unstable... In general, this here above 0 seems unstable... At least the the reg values below 
     'loss_class_alpha' : 0.75, # try with 0.75 # alpha=0.5 even.
     "loss_reg": 'b',
