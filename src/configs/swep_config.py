15989a96e2719c51f8aa8805d440329871890df9
Polichinel <simmaa@prio.org>
2023-02-21 15:20:49 +0100

    using torch BCE

--- src/configs/swep_config.py
+++ src/configs/swep_config.py
@@ -11,10 +11,10 @@ def get_swep_config():
     sweep_config['metric'] = metric
 
     parameters_dict = {
-        'model' : {'value' : 'BNUNet'},
+        'model' : {'value' : 'HydraBNUNet'},
         'clip_grad_norm' : {'value': True},
         'scheduler' : {'value': 'step'},
-        'hidden_channels': {'values': [16, 32]},
+        'hidden_channels': {'values': [16, 32]}, # you like need 32, it seems from qualitative results
         'min_events': {'value': 10},
         'samples': {'value': 800},
         "dropout_rate" : {'value' : 0.05},
@@ -22,14 +22,14 @@ def get_swep_config():
         "weight_decay" : {'value' : 0.1},
         'input_channels' : {'value' : 3},
         'output_channels': {'value' : 3},
-        'loss_class' : { 'value' :  'b'}, # det nytter jo ikke noget at du køre over gamma og alpha for loss-class a...
-        'loss_class_gamma' : { 'values' : [0, 0.5, 1, 2, 5]},
-        'loss_class_alpha' : { 'values' : [0.5, 0.75, 0.95]}, # should be between 0.5 and 0.95...
+        'loss_class' : { 'value' :  'a'}, # det nytter jo ikke noget at du køre over gamma og alpha for loss-class a...
+        'loss_class_gamma' : { 'value' : 0},
+        'loss_class_alpha' : { 'value' : 0.5}, # should be between 0.5 and 0.95...
         'loss_reg' : { 'value' : 'b'},
-        'loss_reg_a' : { 'values' : [0.5, 1, 1.5]},
-        'loss_reg_c' : { 'values' : [0.05, 0.1]},
+        'loss_reg_a' : { 'values' : [0.1, 0.5, 1, 1.5]},
+        'loss_reg_c' : { 'values' : [0.01, 0.05, 0.1]},
         'test_samples': { 'value' : 128},
-        'h_init' : {'value' : 'zero'}  # right now this is just as a note to self. Can't change it here     
+        'h_init' : {'value' : 'abs_rand_exp-100'}  # right now this is just as a note to self. Can't change it here     
         }
 
 
