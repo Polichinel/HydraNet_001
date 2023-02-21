762540d3942ed9106a092f02e221191294e9dbcf
Polichinel <simmaa@prio.org>
2023-02-21 14:34:21 +0100

    lower a nad c, bacis alpha and gamma

--- src/configs/hyperparameters_config.py
+++ src/configs/hyperparameters_config.py
@@ -13,12 +13,12 @@ def get_hp_config():
     "weight_decay" :  0.1,
     'input_channels' : 3,
     "output_channels" : 3,
-    "loss_class": 'b',
-    'loss_class_gamma' : 2, # try with 1 here and 0.75 below
-    'loss_class_alpha' : 0.5,
+    "loss_class": 'b', 
+    'loss_class_gamma' : 0, # try with 1 here and 0.75 below # # gamma=0 no wieght
+    'loss_class_alpha' : 0.5, # try with 0.75 # alpha=0.5 even.
     "loss_reg": 'b',
-    'loss_reg_a' : 5,
-    'loss_reg_c' :  0.1,
+    'loss_reg_a' : 0.5, # new 
+    'loss_reg_c' :  0.05, # new
     "test_samples": 128}
 
     return hyperparameters
