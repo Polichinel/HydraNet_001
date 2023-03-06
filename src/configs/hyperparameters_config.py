a6817093e88f6a2b3a60411fccaa7b729be357c8
Polichinel <simmaa@prio.org>
2023-03-06 11:20:58 +0100

    try this

--- src/configs/hyperparameters_config.py
+++ src/configs/hyperparameters_config.py
@@ -17,7 +17,7 @@ def get_hp_config():
     'loss_class_gamma' : 2, # try with 1 here and 0.75 below # # gamma=0 no wieght. Between 0 and 1 seems very unstable... In general, this here above 0 seems unstable... At least the the reg values below 
     'loss_class_alpha' : 0.5, # try with 0.75 # alpha=0.5 even.
     "loss_reg": 'b',
-    'loss_reg_a' : 8, # new 
+    'loss_reg_a' : 10, # new 
     'loss_reg_c' :  0.05, # new
     "test_samples": 128}
 
