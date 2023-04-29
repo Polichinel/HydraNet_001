c20d8b7894abde9343f48b05f40cb345d57661ac
Polichinel <simmaa@prio.org>
2023-04-29 14:21:15 +0200

    even loweer learning rate...and one more

--- src/configs/swep_config.py
+++ src/configs/swep_config.py
@@ -16,9 +16,9 @@ def get_swep_config():
         'scheduler' : {'value': 'linear'}, # 'OneCycleLR'
         'hidden_channels': {'value': 32}, # you like need 32, it seems from qualitative results
         'min_events': {'value': 10},
-        'samples': {'value': 256}, # just speed running here..
+        'samples': {'values': [256, 512, 1024]}, # just speed running here..
         "dropout_rate" : {'value' : 0.05},
-        'learning_rate': {'value' : 0.0001},
+        'learning_rate': {'value' : 0.00001},
         "weight_decay" : {'value' : 0.1},
         'input_channels' : {'value' : 3},
         'output_channels': {'value' : 3},
@@ -30,7 +30,7 @@ def get_swep_config():
         'loss_reg_c' : { 'value' : 0.01},
         'test_samples': { 'value' : 128},
         #'start_months' :{'values' : [1,2,4,6,8,12]},
-        'seed' : {'values' : [1,2,3,4,5]},
+        'seed' : {'values' : [1,2,3]},
         'h_init' : {'value' : 'abs_rand_exp-100'}  # right now this is just as a note to self. Can't change it here     
         }
 
