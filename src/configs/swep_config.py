6c4234664c19827f48d1224ca9f7f87ce0a96089
Polichinel <simmaa@prio.org>
2023-05-02 00:01:48 +0200

    new sweep

--- src/configs/swep_config.py
+++ src/configs/swep_config.py
@@ -16,7 +16,7 @@ def get_swep_config():
         'scheduler' : {'value': 'linear'}, # 'OneCycleLR'
         'hidden_channels': {'value': 32}, # you like need 32, it seems from qualitative results
         'min_events': {'value': 10},
-        'samples': {'value': 1024}, # just speed running here..
+        'samples': {'values': [252,1024]}, # just speed running here..
         "dropout_rate" : {'value' : 0.05},
         'learning_rate': {'value' : 0.0001},
         "weight_decay" : {'value' : 0.1},
@@ -31,7 +31,7 @@ def get_swep_config():
         'test_samples': { 'value' : 128},
         #'start_months' :{'values' : [1,2,4,6,8,12]},
         'seed' : {'values' : [1,2,3,4,5]},
-        'dim' : {'values' : [64,32,16,128]},
+        'dim' : {'values' : [32,16]},
         'h_init' : {'value' : 'abs_rand_exp-100'}  # right now this is just as a note to self. Can't change it here     
         }
 
