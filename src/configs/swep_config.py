73d37c19c2229f08abf84e577d3b9f208e577552
Polichinel <simmaa@prio.org>
2023-05-03 09:13:43 +0200

    correction

--- src/configs/swep_config.py
+++ src/configs/swep_config.py
@@ -16,7 +16,7 @@ def get_swep_config():
         'scheduler' : {'values': ['linear', 'OneCycleLR']}, # 'OneCycleLR'
         'hidden_channels': {'value': 32}, # you like need 32, it seems from qualitative results
         'min_events': {'value': 10},
-        'samples': {'values': [252, 1024, 2048]}, # just speed running here..
+        'samples': {'values': [1024, 2048]}, # just speed running here..
         "dropout_rate" : {'value' : 0.05},
         'learning_rate': {'value' : 0.0001},
         "weight_decay" : {'value' : 0.1},
