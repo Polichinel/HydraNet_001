5e94d46d462034c86aee30a9f070ebe6f850a501
Polichinel <spm@ifs.ku.dk>
2023-06-10 10:00:58 +0200

    no wierd shift

--- src/configs/swep_config.py
+++ src/configs/swep_config.py
@@ -16,22 +16,22 @@ def get_swep_config():
         'scheduler' : {'value': 'linear'}, # 'OneCycleLR'
         'hidden_channels': {'value': 32}, # you like need 32, it seems from qualitative results
         'min_events': {'value': 5},
-        'samples': {'values': [500, 1000, 1500]}, # just speed running here..
+        'samples': {'value':  1000}, # just speed running here..
         "dropout_rate" : {'value' : 0.05},
-        'learning_rate': {'values' : [0.005, 0.001, 0.0005]},
+        'learning_rate': {'values' : [0.0005, 0.0001]},
         "weight_decay" : {'value' : 0.1},
         "slope_ratio" : {'value' : 0.75},
         'input_channels' : {'value' : 3},
         'output_channels': {'value' : 3},
-        'loss_class' : { 'value' : 'd'}, # det nytter jo ikke noget at du køre over gamma og alpha for loss-class a...
+        'loss_class' : { 'value' : 'c'}, # det nytter jo ikke noget at du køre over gamma og alpha for loss-class a...
         'loss_class_gamma' : {'value' : 2},
         'loss_class_alpha' : {'value' : 0.5}, # should be between 0.5 and 0.95...
-        'loss_reg' : { 'value' : 'c', },
+        'loss_reg' : { 'value' : 'b', },
         'loss_reg_a' : { 'value' : 14},
         'loss_reg_c' : { 'value' : 0.01},
         'test_samples': { 'value' : 128},
         #'start_months' :{'values' : [1,2,4,6,8,12]},
-        'seed' : {'values' : [4,5]},
+        'seed' : {'values' : [3,4,5]},
         #'dim' : {'values' : [32,16]},
         'h_init' : {'value' : 'abs_rand_exp-100'}  # right now this is just as a note to self. Can't change it here     
         }
