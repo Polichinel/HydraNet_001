164ce35d99d890eef985504ea4a8e1bb9c63fce4
Polichinel <simmaa@prio.org>
2023-06-16 12:13:19 +0200

    new sweep higher batch size

--- src/configs/swep_config.py
+++ src/configs/swep_config.py
@@ -16,8 +16,8 @@ def get_swep_config():
         'scheduler' : {'value': 'linear'}, # 'OneCycleLR'
         'hidden_channels': {'value': 32}, # you like need 32, it seems from qualitative results
         'min_events': {'value': 5},
-        'samples': {'values':   [256, 512]}, # should be a function of batches becaus batch 3 and sample 1000 = 3000....
-        'batch_size': {'value':  3}, # just speed running here..
+        'samples': {'values':   [512, 777]}, # should be a function of batches becaus batch 3 and sample 1000 = 3000....
+        'batch_size': {'values':  [3, 6, 9]}, # just speed running here..
         "dropout_rate" : {'value' : 0.05},
         'learning_rate': {'values' : [0.005, 0.001]},
         "weight_decay" : {'value' : 0.1},
@@ -33,8 +33,8 @@ def get_swep_config():
         'loss_reg_c' : { 'value' : 0.01},
         'test_samples': { 'value' : 128},
         #'start_months' :{'values' : [1,2,4,6,8,12]},
-        'np_seed' : {'values' : [3, 4, 5]},
-        'torch_seed' : {'values' : [3, 4, 5]},
+        'np_seed' : {'values' : [3, 4]},
+        'torch_seed' : {'values' : [3, 4]},
         #'dim' : {'values' : [32,16]},
         'h_init' : {'value' : 'abs_rand_exp-100'}  # right now this is just as a note to self. Can't change it here     
         }
