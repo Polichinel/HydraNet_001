45f1e2043523e806e02c4dfc336962e652aa78e0
Polichinl <simmaa@prio.org>
2023-09-21 09:18:20 +0200

    'loss_class_gamma' : 0.5 loss_class_alpha: 0.5

--- src/configs/hyperparameters_config.py
+++ src/configs/hyperparameters_config.py
@@ -17,8 +17,8 @@ def get_hp_config():
     'input_channels' : 3,
     'output_channels' : 3,
     'loss_class': 'd',  # band c are is still unstable... c is old, d = FocalLoss_new
-    'loss_class_gamma' : 2, # 2 works and 0 works. , # try with 1 here and 0.75 below # # gamma=0 no wieght. Between 0 and 1 seems very unstable... In general, this here above 0 seems unstable... At least the the reg values below 
-    'loss_class_alpha' : 0.25, # try with 0.75 # alpha=0.5 even.
+    'loss_class_gamma' : 0.5, # 2 works and 0 works. , # try with 1 here and 0.75 below # # gamma=0 no wieght. Between 0 and 1 seems very unstable... In general, this here above 0 seems unstable... At least the the reg values below 
+    'loss_class_alpha' : 0.5, # try with 0.75 # alpha=0.5 even.
     'loss_reg': 'b',
     'loss_reg_a' : 14, 
     'loss_reg_c' :  0.01, # 0.05 works...
