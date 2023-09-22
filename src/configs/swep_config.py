1f1158023a54da35ba87aa35c997edd9dbaab82b
Polichinl <simmaa@prio.org>
2023-09-23 01:19:42 +0200

    this?

--- src/configs/swep_config.py
+++ src/configs/swep_config.py
@@ -30,7 +30,7 @@ def get_swep_config():
         'loss_class_alpha' : {'value' : 0.75}, # should be between 0.5 and 0.95...
         'loss_reg' : { 'value' : 'b', },
         'loss_reg_a' : { 'values' : [7, 14, 21]},
-        'loss_reg_c' : { 'values' : [0.01, 0.1]},
+        'loss_reg_c' : { 'value' : 0.01},
         'test_samples': { 'value' : 128},
         #'start_months' :{'values' : [1,2,4,6,8,12]},
         'np_seed' : {'values' : [3,4]},
