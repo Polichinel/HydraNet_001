24488b7efc528d79a4ef14d8f801646b2e5069d9
Polichinl <simmaa@prio.org>
2023-09-22 15:58:04 +0200

    HydraBNUNet06

--- src/configs/swep_config.py
+++ src/configs/swep_config.py
@@ -11,14 +11,14 @@ def get_swep_config():
     sweep_config['metric'] = metric
 
     parameters_dict = {
-        'model' : {'value' :'HydraBNUNet03'},
+        'model' : {'value' :'HydraBNUNet06'},
         'clip_grad_norm' : {'value': True},
         'scheduler' : {'value': 'linear'}, # 'OneCycleLR'
         'hidden_channels': {'value': 32}, # you like need 32, it seems from qualitative results
         'min_events': {'value': 5},
         'samples': {'values':   [512, 1024]}, # should be a function of batches becaus batch 3 and sample 1000 = 3000....
-        'batch_size': {'values':  [3,6,9]}, # just speed running here..
-        "dropout_rate" : {'value' : 0.05},
+        'batch_size': {'value':  3}, # just speed running here..
+        "dropout_rate" : {'values' : [0.05,0.1,0,25]},
         'learning_rate': {'value' :  0.001}, #0.001 default, but 0.005 might be better
         "weight_decay" : {'value' : 0.1},
         "slope_ratio" : {'value' : 0.75},
@@ -26,11 +26,11 @@ def get_swep_config():
         'input_channels' : {'value' : 3},
         'output_channels': {'value' : 3},
         'loss_class' : { 'value' : 'c'}, # det nytter jo ikke noget at du køre over gamma og alpha for loss-class a...
-        'loss_class_gamma' : {'value' : 2},
-        'loss_class_alpha' : {'value' : 0.5}, # should be between 0.5 and 0.95...
+        'loss_class_gamma' : {'value' : 1.5},
+        'loss_class_alpha' : {'value' : 0.75}, # should be between 0.5 and 0.95...
         'loss_reg' : { 'value' : 'b', },
-        'loss_reg_a' : { 'value' : 14},
-        'loss_reg_c' : { 'value' : 0.01},
+        'loss_reg_a' : { 'values' : [7, 14, 21]},
+        'loss_reg_c' : { 'values' : [0.01, 0.1, 0.5, 0.9]},
         'test_samples': { 'value' : 128},
         #'start_months' :{'values' : [1,2,4,6,8,12]},
         'np_seed' : {'values' : [3,4]},
