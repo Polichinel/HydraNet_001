dec93fcb64cfb92bf0f72c2cad5e4cf61f331515
Polichinl <simmaa@prio.org>
2023-10-04 12:13:28 +0200

    'samples': {'values':   [300, 400, 500]}

--- src/configs/swep_config.py
+++ src/configs/swep_config.py
@@ -17,7 +17,7 @@ def get_swep_config():
         'scheduler' : {'values': ['CosineAnnealingLR', 'linear']}, # 'OneCycleLR'
         'hidden_channels': {'value': 32}, # you like need 32, it seems from qualitative results
         'min_events': {'value': 5},
-        'samples': {'values':   [300, 400]}, # should be a function of batches becaus batch 3 and sample 1000 = 3000....
+        'samples': {'values':   [300, 400, 500]}, # should be a function of batches becaus batch 3 and sample 1000 = 3000....
         'batch_size': {'value':  3}, # just speed running here..
         "dropout_rate" : {'values' : [0.125, 0.25]},
         'learning_rate': {'values' :  [0.001, 0.0005]}, #0.001 default, but 0.005 might be better
