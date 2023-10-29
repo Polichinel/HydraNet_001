9d5830f79683ce04fbd64f82c408822955f21fd1
Polichinl <simmaa@prio.org>
2023-10-29 14:10:23 +0100

    'non_logged' : False

--- src/configs/hyperparameters_config.py
+++ src/configs/hyperparameters_config.py
@@ -29,7 +29,7 @@ def get_hp_config():
     'window_dim' : 32,
     'loss_distance_scale' : 0.001,  # right now this is just as a note to self. Can't change it here     
     'h_init' : 'abs_rand_exp-100',
-    'non_logged' : True, # right now this is just as a note to self. Can't change it here} and it is not true..
+    'non_logged' : False, # right now this is just as a note to self. Can't change it here} and it is not true..
     'warmup_steps' : 100,
     'first_feature_idx' : 5,
     }
