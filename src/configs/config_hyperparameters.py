2b63ae876a9ddf7d238b54782f265ababf0fec44
Polichinl <simmaa@prio.org>
2024-02-14 20:45:07 +0100

    full run

--- src/configs/config_hyperparameters.py
+++ src/configs/config_hyperparameters.py
@@ -8,7 +8,7 @@ def get_hp_config():
     'scheduler' : 'WarmupDecay', #  'CosineAnnealingLR' 'OneCycleLR'
     'total_hidden_channels' : 32,
     'min_events' : 5,
-    'samples': 10, # 10 just for debug
+    'samples': 600, # 10 just for debug
     'batch_size': 3, 
     'dropout_rate' : 0.125,
     'learning_rate' :  0.001,
